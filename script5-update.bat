8324bd64044b6f3f19173b340bec666d0a300418
Carlos Falcone <carloseduardo.falcone@gmail.com>
2024-01-31 14:18:17 -0300

    update3

--- script5-update.bat
+++ script5-update.bat
@@ -1,14 +1,12 @@
 REM Controle de versao das imagens docker
-SET TAG_BACKFRONT=10
-SET TAG_DATABASE=7
-
+SET TAG_BACKFRONT=13
+SET TAG_DATABASE=10
 
 REM Atualizar as tags nos arquivos YML
-
-powershell -Command "(Get-Content .github/workflows/cicd.yml) -replace '%%TAG_BACKFRONT%%', '%TAG_BACKFRONT%' -replace '%%TAG_DATABASE%%', '%TAG_DATABASE%' | Set-Content .github\workflows\cicd.yml"
-powershell -Command "(Get-Content deployment.yml) -replace '%%TAG_BACKFRONT%%', '%TAG_BACKFRONT%' -replace '%%TAG_DATABASE%%', '%TAG_DATABASE%' | Set-Content deployment.yml"
+powershell -Command "(Get-Content .\arquivos_base\cicd.yml) -replace '%%TAG_BACKFRONT%%', '%TAG_BACKFRONT%' -replace '%%TAG_DATABASE%%', '%TAG_DATABASE%' | Set-Content .\.github\workflows\cicd.yml"
+powershell -Command "(Get-Content .\arquivos_base\deployment.yml) -replace '%%TAG_BACKFRONT%%', '%TAG_BACKFRONT%' -replace '%%TAG_DATABASE%%', '%TAG_DATABASE%' | Set-Content deployment.yml"
 
 REM Subir os arquivos atualizados para o GitHub
 git add .
-git commit -m "update2"
+git commit -m "update3"
 git push -u origin main
