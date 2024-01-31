335b3e70b991063e4724b7d315e7786e22455b94
Carlos Falcone <carloseduardo.falcone@gmail.com>
2024-01-31 13:21:02 -0300

    update2

--- script5-update.bat
+++ script5-update.bat
@@ -5,10 +5,10 @@ SET TAG_DATABASE=7
 
 REM Atualizar as tags nos arquivos YML
 
-powershell -Command "(Get-Content .github/workflows/cicd.yml) -replace '%%TAG_BACKFRONT%%', '%TAG_BACKFRONT%' -replace '%%TAG_DATABASE%%', '%TAG_DATABASE%' | Set-Content cicd.yml"
+powershell -Command "(Get-Content .github/workflows/cicd.yml) -replace '%%TAG_BACKFRONT%%', '%TAG_BACKFRONT%' -replace '%%TAG_DATABASE%%', '%TAG_DATABASE%' | Set-Content .github\workflows\cicd.yml"
 powershell -Command "(Get-Content deployment.yml) -replace '%%TAG_BACKFRONT%%', '%TAG_BACKFRONT%' -replace '%%TAG_DATABASE%%', '%TAG_DATABASE%' | Set-Content deployment.yml"
 
 REM Subir os arquivos atualizados para o GitHub
 git add .
-git commit -m "update1"
+git commit -m "update2"
 git push -u origin main
